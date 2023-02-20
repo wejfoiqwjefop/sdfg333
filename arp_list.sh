9e72f4c291d1499b44b114ff7397eda2d743a41d
wejfoiqwjefop <pochamp33331@gmail.com>
2023-02-21 00:50:39 +0300

    asd

--- arp_list.sh
+++ arp_list.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 while true; do
-	LIST=$(arp -i enp0s3)
-#	LIST=$(arp -i enp0s3;date)
-#	echo -e "$LIST\n-----------"   >> /home/alex/workplace/list
+	LIST=$(arp -i eth0)
+#	LIST=$(arp -i eth0;date)
+#	echo -e "$LIST\n-----------"   >> /home/user/workplace/list
 	echo "$LIST"
 	sleep 60
 done
